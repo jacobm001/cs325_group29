70b213653f981593cab1681969be266a670e32ec
Jacob Mastel <jacob@mastel.org>
2015-04-24 14:07:29 -0700

    Wrote algorithm1

--- project1/algorithm1.py
+++ project1/algorithm1.py
@@ -1,2 +1,12 @@
 def algorithm1(A):
-	print "Hello World!"
+	max = 0
+	for i in range(0, len(A)):
+		for j in range(i, len(A)):
+			partial = 0
+			for j in range(i, j):
+				partial += A[j]
+
+			if partial > max:
+				max = partial
+
+	return max
