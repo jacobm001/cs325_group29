f6f353c9c12fdce852fb5fbc56d530ea417e0857
Jacob Mastel <jacob@mastel.org>
2015-04-24 16:30:51 -0700

    algorithm2 implemented

--- project1/algorithm2.py
+++ project1/algorithm2.py
@@ -1,2 +1,10 @@
 def algorithm2(A):
-	print "Hello World!"
+	max = 0
+	for i in range(0, len(A)):
+		sum = 0
+		for j in range(i, len(A)):
+			sum = sum + A[j]
+			if max <= sum:
+				max = sum
+
+	return max
