05658165c5bc3813985caa9d300671a387d23f8c
jacob <jacob@mastel.org>
2015-05-18 12:57:43 -0700

    added data points

--- project3/q2.m
+++ project3/q2.m
@@ -14,3 +14,42 @@ lettuce[cost]    = 0.75
 
 spinach[energy]  = 40
 spinach[protein] = 2.86
+spinach[fat]     = 0.39
+spinach[carbs]   = 3.63
+spinach[sodium]  = 65.00
+spinach[cost]    = 0.50
+
+carrot[energy]  = 41
+carrot[protein] = 0.93
+carrot[fat]     = 0.24
+carrot[carbs]   = 9.58
+carrot[sodium]  = 69.00
+carrot[cost]    = 0.50
+
+sunflower[energy]  = 585
+sunflower[protein] = 23.4
+sunflower[fat]     = 48.7
+sunflower[carbs]   = 15.00
+sunflower[sodium]  = 3.80
+sunflower[cost]    = 0.45
+
+tofu[energy]  = 120
+tofu[protein] = 16.00
+tofu[fat]     = 5.00
+tofu[carbs]   = 3.00
+tofu[sodium]  = 120.00
+tofu[cost]    = 2.15
+
+chickpeas[energy]  = 164
+chickpeas[protein] = 9.00
+chickpeas[fat]     = 2.6
+chickpeas[carbs]   = 27.00
+chickpeas[sodium]  = 78.00
+chickpeas[cost]    = 0.95
+
+oil[energy]  = 884
+oil[protein] = 0
+oil[fat]     = 100.00
+oil[carbs]   = 0
+oil[sodium]  = 0
+oil[cost]    = 2.00
