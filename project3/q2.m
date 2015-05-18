1664fcbff33cb4df8b9f48c9ab3c069544cb5527
Jacob Mastel <jacobm001@gmail.com>
2015-05-18 12:51:33 -0700

    started question 2

---
+++ project3/q2.m
@@ -0,0 +1,16 @@
+tomatoe[energy]  = 21
+tomatoe[protein] = 0.85
+tomatoe[fat]     = 0.33
+tomatoe[carbs]   = 4.64
+tomatoe[sodium]  = 9.00
+tomatoe[cost]    = 1.00
+
+lettuce[energy]  = 16
+lettuce[protein] = 1.62
+lettuce[fat]     = 0.20
+lettuce[carbs]   = 2.37
+lettuce[sodium]  = 28.00
+lettuce[cost]    = 0.75
+
+spinach[energy]  = 40
+spinach[protein] = 2.86
